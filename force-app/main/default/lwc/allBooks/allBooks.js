ac57fbea72009c1c74bbe4d886d9beface892137
Bartlomiej <bartlomiej.bujak95@gmail.com>
2022-05-30 10:24:36 +0200

    changed search in all books page that instead every time sending querry to database now is filtering trough local object

--- force-app/main/default/lwc/allBooks/allBooks.js
+++ force-app/main/default/lwc/allBooks/allBooks.js
@@ -6,29 +6,40 @@ export default class AllBooks extends LightningElement {
     @track books
     searchValue
     error
+    @track booksAfterSearchAndFilter
     async getAllBooks(){
         try{
             this.books=await getBooks();
+            this.booksAfterSearchAndFilter=[...this.books];
         }catch(error){
             this.error=error;
         }
     }
     async searchBooks(){
-        try{
-            this.books=await searchBooks({searchValue: this.searchValue});
-        }catch(error){
-            this.error=error;
-        }
+        this.booksAfterSearchAndFilter=[...this.books.filter(book=>{
+            if(book.Name.toLowerCase().includes(this.searchValue.toLowerCase())||
+            book.Author__c.toLowerCase().includes(this.searchValue.toLowerCase())||
+            book.ISBN__c.toLowerCase().includes(this.searchValue.toLowerCase())){
+                return true
+            }else{
+
+                return false;
+            }
+        })]
     }
     connectedCallback(){
         this.getAllBooks();
+        
     }
     onSearchInputValueChange(event){
+        console.log("onSearchInputValueChange")
         this.searchValue=event.target.value;
         if(this.searchValue==''){
-            this.getAllBooks()
+            this.booksAfterSearchAndFilter=[...this.books];
+        }else{
+
+            this.searchBooks()
         }
-        this.searchBooks()
     }
     sortBySold(){
        
@@ -36,7 +47,7 @@ export default class AllBooks extends LightningElement {
     sortByRecent(){
 
     }
-    sortByAz(){
+    sortByAZ(){
         
     }
 }
